fa02ece8683766afdf62225d1c98c481e173ccad
RSmetanyuk <rsmetanyuk@gmail.com>
2018-10-31 22:14:30 +0200

    Passed tests

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,22 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const numArr = num.toString().split('');
+  const length = numArr.length;
+  let double = false;
+  
+  if (!(length % 2)) {
+    double = true;
+    for (let i = 0; i < length / 2; i++) {
+      if (numArr[i] !== numArr[i + length / 2]) {
+        double = false;
+        break;
+      }
+    }
+  }
+
+  if (double) {
+    return num;
+  } else {
+    return (num * 2);
+  }
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,7 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
-  // your logic here...
+  const arrSorted = arr.sort(function (a, b) {
+    return eval(a.toString().split('').join('+')) - eval(b.toString().split('').join('+'));
+  });
+  return arrSorted;
 };
