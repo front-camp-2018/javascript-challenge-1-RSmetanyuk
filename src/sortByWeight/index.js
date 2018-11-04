2d19b086b9da92656db8a04e8764f9912db99626
RSmetanyuk <rsmetanyuk@gmail.com>
2018-11-04 14:51:00 +0200

    removed eval

--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,10 +1,8 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
-  const sunNumDigits = num => {
-    num.toString().split('').map(Number).reduce((accumulator, currentValue) => {
-      accumulator + currentValue;
-    });
-  };
+  const sunNumDigits = num =>
+    num.toString().split('').map(Number).reduce((accumulator, currentValue) =>
+      accumulator + currentValue);
   
   const arrSorted = arr.sort(function (a, b) {
     return sunNumDigits(a) - sunNumDigits(b);
